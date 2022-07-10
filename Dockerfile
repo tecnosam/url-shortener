4966695931dde75db05a2032cdebc0cc12b214f3
Abolo Samuel <ikabolo59@gmail.com>
2022-07-10 05:26:52 +0100

    commit

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM python:3.8-alpine
 
 # By default, listen on port 5000
-EXPOSE 5000/tcp
+EXPOSE 5000
+EXPOSE 8080/tcp
+EXPOSE 800/tcp
 
 # Set the working directory in the container
 WORKDIR /url-shortener
@@ -13,7 +15,9 @@ COPY .dockerignore .
 
 # Install any dependencies
 RUN pip install -r requirements.txt
+RUN pip install gunicorn
 
 COPY . .
 # Specify the command to run on container start
+# CMD [ "gunicorn", "main:app --bind '0.0.0.0:5000'" ]
 CMD [ "python", "main.py" ]
--- app/routes/url_routes.py
+++ app/routes/url_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, abort, Response
+from flask import Blueprint, redirect, request, abort, Response
 
 from app.models.urls import URL, URLShema
 
@@ -10,11 +10,21 @@ url_schema = URLShema()
 def add_url():
     url = request.form['url']
 
-    _url: URL = URL(url)
+    _url: URL = URL.add(url)
 
     response = url_schema.dump(_url)
 
     return response
+
+@blueprint.route("/<url_endpoint>", methods=['GET'])
+def work_endpoint(url_endpoint):
+    
+    _url: URL = URL.query.filter_by(endpoint=url_endpoint)
+
+    if _url is None:
+        abort(Response("URL not found", 404))
+
+    return redirect(_url.real_link)
 
 @blueprint.route("/<url_id>/edit", methods=['PUT', 'DELETE'])
 def edit_url(url_id):
@@ -32,4 +42,3 @@ def edit_url(url_id):
         _url = _url.pop()
 
     return url_schema.dump(_url)
-
